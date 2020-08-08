5ca58a4b5c4b7ba88cd683a5ddc55917b01d27e9
Igor <9543463@gmail.com>
2020-08-08 06:42:57 -0700

    fix: create map jquery

--- js/main.js
+++ js/main.js
@@ -77,8 +77,8 @@ $(document).ready(function () {
     });
   });
   AOS.init();
-  $('.map').addEventListener('mouseover', () => {
-    this.innerHTML = `
+  $('.map').mouseenter(function () {
+    $('.map').append(
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423282.1813973622!2d-118.41173606237231!3d34.02098198399712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2be4e9ec210a7%3A0x98c2af521186a3cd!2sHilton%20Los%20Angeles%2FUniversal%20City!5e0!3m2!1sen!2sus!4v1595634525408!5m2!1sen!2sus"
         width="100%"
@@ -89,6 +89,6 @@ $(document).ready(function () {
         aria-hidden="false"
         tabindex="0"
       ></iframe>
-    `;
+    );
   });
 });
